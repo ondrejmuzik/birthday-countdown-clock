0053406618d0dd72fa67af68fe7e35aa05325399
Ondrej Muzik <muzik@ondrejmuzik.cz>
2026-02-09 21:36:47 +0100

    Icon rotation added

--- birthday_countdown_clock.ino
+++ birthday_countdown_clock.ino
@@ -35,6 +35,13 @@ const unsigned long countdownTimeout = 30000;  // 30 seconds
 const int clockIntensity = 0;
 const int countdownIntensity = 3;
 
+// Icon animation
+const unsigned long iconInterval = 3000;  // 3 seconds
+unsigned long lastIconSwitch = 0;
+int iconIndex = 0;
+const char birthdayIcons[] = {'@', '~', '#'};   // cake, present, heart
+const char christmasIcons[] = {'^', '*', '~'};   // tree, star, present
+
 // Custom V character (maps to 'V')
 const uint8_t vChar[] = {
   5,
@@ -68,6 +75,19 @@ const uint8_t cakeChar[] = {
   B00000000,
 };
 
+// Heart character (maps to '#')
+const uint8_t heartChar[] = {
+  8,
+  B00001100,
+  B00010010,
+  B00100010,
+  B01000100,
+  B00100010,
+  B00010010,
+  B00001100,
+  B00000000,
+};
+
 // Custom present character (maps to '~')
 const uint8_t presentChar[] = {
   8,
@@ -91,20 +111,20 @@ const uint8_t treeChar[] = {
   B00111110,
   B00110100,
   B00100000,
-  B00100000,
+  B00000000,
 };
 
 // Star character (maps to '&')
 const uint8_t starChar[] = {
   8,
   B00001000,
-  B01011000,
-  B00100100,
-  B00010010,
-  B00100100,
-  B01011000,
+  B00101010,
+  B00011100,
+  B01111111,
+  B00011100,
+  B00101010,
   B00001000,
-  B00100000,
+  B00000000
 };
 
 void setup() {
@@ -128,6 +148,7 @@ void setup() {
   myDisplay.addChar('V', vChar);
   myDisplay.addChar('B', bChar);
   myDisplay.addChar('@', cakeChar);
+  myDisplay.addChar('#', heartChar);
 
   // Initialize RTC
   if (!rtc.begin()) {
@@ -163,6 +184,8 @@ void loop() {
       } else {
         displayMode = 1;  // Show birthday countdown
         myDisplay.setIntensity(countdownIntensity);
+        iconIndex = 0;
+        lastIconSwitch = millis();
         countdownStartTime = millis();
       }
       lastDebounceTime = millis();
@@ -180,6 +203,8 @@ void loop() {
       } else {
         displayMode = 2;  // Show July 28 countdown
         myDisplay.setIntensity(countdownIntensity);
+        iconIndex = 0;
+        lastIconSwitch = millis();
         countdownStartTime = millis();
       }
       lastDebounceTime = millis();
@@ -197,6 +222,8 @@ void loop() {
       } else {
         displayMode = 3;  // Show Christmas countdown
         myDisplay.setIntensity(countdownIntensity);
+        iconIndex = 0;
+        lastIconSwitch = millis();
         countdownStartTime = millis();
       }
       lastDebounceTime = millis();
@@ -211,6 +238,12 @@ void loop() {
     lastDisplayedMinute = -1;  // Force time update
   }
   
+  // Rotate icons every 3 seconds in countdown modes
+  if (displayMode != 0 && (millis() - lastIconSwitch >= iconInterval)) {
+    iconIndex = (iconIndex + 1) % 3;
+    lastIconSwitch = millis();
+  }
+
   // Display based on current mode
   switch (displayMode) {
     case 0:  // Time mode - only update when minute changes
@@ -222,30 +255,30 @@ void loop() {
       break;
       
     case 1:  // V's birthday (May 30)
-      displayBirthdayCountdown(now, 5, 30, "V");
+      displayBirthdayCountdown(now, 5, 30, "V", birthdayIcons[iconIndex]);
       break;
 
     case 2:  // B's birthday (July 28)
-      displayBirthdayCountdown(now, 7, 28, "B");
+      displayBirthdayCountdown(now, 7, 28, "B", birthdayIcons[iconIndex]);
       break;
-      
+
     case 3:  // Christmas (Dec 24)
-      displayChristmasCountdown(now, 12, 24);
+      displayChristmasCountdown(now, 12, 24, christmasIcons[iconIndex]);
       break;
   }
   
   delay(100);
 }
 
-void displayChristmasCountdown(DateTime now, int month, int day) {
+void displayChristmasCountdown(DateTime now, int month, int day, char icon) {
   int daysUntil = calculateDaysUntil(now, month, day);
-  sprintf(displayBuffer, "^  %3d", daysUntil);
+  sprintf(displayBuffer, "%c  %3d", icon, daysUntil);
   myDisplay.print(displayBuffer);
 }
 
-void displayBirthdayCountdown(DateTime now, int month, int day, const char* name) {
+void displayBirthdayCountdown(DateTime now, int month, int day, const char* name, char icon) {
   int daysUntil = calculateDaysUntil(now, month, day);
-  sprintf(displayBuffer, "%s@%3d", name, daysUntil);
+  sprintf(displayBuffer, "%s%c%3d", name, icon, daysUntil);
   myDisplay.print(displayBuffer);
 }
 
